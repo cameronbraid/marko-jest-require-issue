9e86c1645cf2e289d600772abaf7ef2a32d9d6ee
Cameron Braid <cameron@drivenow.com.au>
2018-05-22 23:02:09 +1000

    fixup

--- require.test.js
+++ require.test.js
@@ -1,10 +1,16 @@
 'use strict';
-require('marko/node-require')
+//require('marko/node-require') // not needed since we are using jest-marko.js transformer
 let marko = require("marko")
 let Parent = marko.load(require.resolve("./components/Parent.marko"))
 let Parent2 = require("./components/Parent.marko")
 describe("test", function(){
-  it('test', function() {
+  it('load vs load', function() {
+    expect(Parent.renderToString({})).toEqual(Parent.renderToString({}))
+  })
+  it('require vs require', function() {
+    expect(Parent2.renderToString({})).toEqual(Parent2.renderToString({}))
+  })
+  it('require vs load', function() {
     expect(Parent2.renderToString({})).toEqual(Parent.renderToString({}))
   })
 })
